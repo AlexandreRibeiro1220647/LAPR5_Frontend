bb44702d1dcf1fc7822d04d93ec6ff754431d514
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-11-11 08:52:57 +0000

    Implementation of an "under development" page regarding routes still not developed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
 import {HomeComponent} from './components/home/home.component';
 import {PatientComponent} from './components/patient/patient.component';
 import {StaffComponent} from './components/staff/staff.component';
 import {AdminComponent} from './components/admin/admin.component';
+import {UnderDevelopmentComponent} from './components/under-dev/under-development.component';
 
 export const routes: Routes = [
+  {path : 'under-dev', component: UnderDevelopmentComponent},
   { path: 'staff', component: StaffComponent },
   { path: 'patient', component: PatientComponent },
   { path: 'admin', component: AdminComponent },
-  { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
 ];
