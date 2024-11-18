5ca3af11c1d509967e3fdc91afbbb6d3a3017d6e
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-11-18 18:30:21 +0000

    Sign Up - Patient Creation DTO

---
+++ src/app/models/patients/createPatientDTO.ts
@@ -0,0 +1,10 @@
+export interface CreatePatientDTO {
+  fullName: string;
+  dateOfBirth: string[];
+  gender: string;
+  contactInformation: string;
+  email: string;
+  medicalConditions: string[];
+  emergencyContact: string;
+  appointmentHistory: string[];
+}
