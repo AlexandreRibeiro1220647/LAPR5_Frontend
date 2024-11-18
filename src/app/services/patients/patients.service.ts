b4ec974ce0e2ee1a19769992dd690f16db7f0567
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-11-18 18:31:00 +0000

    Sign Up - Patient Service for Patient Registration

---
+++ src/app/services/patients/patients.service.ts
@@ -0,0 +1,19 @@
+import {Inject, Injectable} from '@angular/core';
+import {catchError, Observable, Subscription, throwError} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {CreatePatientDTO} from '../../models/patients/createPatientDTO';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientsService {
+
+  constructor(private http: HttpClient, @Inject('API_URL') private apiUrl: string) { }
+
+  createItem(patient: CreatePatientDTO): Observable<any> {
+    console.log("post to create. DTO: ", patient);
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.http.post<any>(`${this.apiUrl}/Patients/register/patient`, patient, { headers, responseType: 'text' as 'json' });
+  }
+}
