a340a33f7f0e8c668cc0ddb02eddcebe7fea01db
andre <1220633@isep.ipp.pt>
2024-11-23 22:42:14 +0000

    tests for the CreateDialog

--- src/app/components/dialog/operation-requests/create/operation-request-dialog-create.spec.ts
+++ src/app/components/dialog/operation-requests/create/operation-request-dialog-create.spec.ts
@@ -1,24 +1,71 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { of } from 'rxjs';
 import { OperationRequestDialogCreate } from './operation-request-dialog-create';
-import { beforeEach, describe, it } from 'node:test';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { OperationRequestService } from '../../../../services/operation-requests/operation-requests.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('CreateComponent', () => {
+// Mock Service
+class MockOperationRequestService {
+  getItems = jasmine.createSpy('getItems').and.returnValue(of([]));
+  getPatients = jasmine.createSpy('getPatients').and.returnValue(of([]));
+  getOperationTypes = jasmine.createSpy('getOperationTypes').and.returnValue(of([]));
+  getStaff = jasmine.createSpy('getStaff').and.returnValue(of([]));
+}
+
+// Mock MatDialogRef
+class MockMatDialogRef<T> {
+  close = jasmine.createSpy('close');
+}
+
+describe('OperationRequestDialogCreate', () => {
   let component: OperationRequestDialogCreate;
   let fixture: ComponentFixture<OperationRequestDialogCreate>;
+  let mockService: MockOperationRequestService;
+  let mockDialogRef: MockMatDialogRef<OperationRequestDialogCreate>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OperationRequestDialogCreate]
-    })
-    .compileComponents();
+      imports: [
+        OperationRequestDialogCreate,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatButtonModule,
+        BrowserAnimationsModule 
+      ],
+      providers: [
+        FormBuilder,
+        { provide: MatDialogRef, useClass: MockMatDialogRef },
+        { provide: OperationRequestService, useClass: MockOperationRequestService }
+      ]
+    }).compileComponents();
 
+    mockService = TestBed.inject(OperationRequestService) as any;
+    mockDialogRef = TestBed.inject(MatDialogRef) as any;
     fixture = TestBed.createComponent(OperationRequestDialogCreate);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call dialogRef.close on cancel', () => {
+    component.onCancel();
+    expect(mockDialogRef.close).toHaveBeenCalled();
+  });
+
+  it('should call dialogRef.close if form is invalid on submit', () => {
+    component.onSubmit();
+    expect(mockDialogRef.close).not.toHaveBeenCalled();
+  });
+
 });
