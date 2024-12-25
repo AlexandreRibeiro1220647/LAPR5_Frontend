bc661d9fb8f39a04b1eba00631f9c27e9d894cac
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-12-25 21:52:08 +0000

    #72 Create Medical Condition Button Implementation

--- src/app/components/admin/allergies-medical-conditions/allergies-medical-conditions.component.ts
+++ src/app/components/admin/allergies-medical-conditions/allergies-medical-conditions.component.ts
@@ -4,6 +4,9 @@ import {MatDialog} from '@angular/material/dialog';
 import {AllergiesService} from '../../../services/allergies/allergies.service';
 import {AllergyDTO} from '../../../models/allergy/allergyDTO';
 import {AllergiesDialogComponent} from '../../dialog/allergies/allergies-dialog.component';
+import {MedicalConditionsService} from '../../../services/medical-conditions/medical-conditions.service';
+import {MedicalConditionsDialogComponent} from '../../dialog/medical-conditions/medical-conditions-dialog.component';
+import {MedicalConditionDTO} from '../../../models/medical-condition/medicalConditionDTO';
 
 @Component({
   selector: 'app-admin',
@@ -16,7 +19,7 @@ import {AllergiesDialogComponent} from '../../dialog/allergies/allergies-dialog.
 })
 export class AllergiesMedicalConditionsComponent {
 
-  constructor(private allergiesService: AllergiesService, public dialog: MatDialog) {
+  constructor(private allergiesService: AllergiesService, private medicalConditionsService: MedicalConditionsService, public dialog: MatDialog) {
   }
 
   openCreateAllergyDialog(): void {
@@ -39,4 +42,23 @@ export class AllergiesMedicalConditionsComponent {
     });
   }
 
+  openCreateMedicalConditionDialog(): void {
+    const dialogRef = this.dialog.open(MedicalConditionsDialogComponent, {
+      width: '400px',
+    });
+    dialogRef.afterClosed().subscribe((result: MedicalConditionDTO | undefined) => {
+      if (result) {
+        // Handle the result data here, e.g., add it to your data array
+        this.medicalConditionsService.createItem(result).subscribe({
+          next: (response) => {
+            console.log('Medical Condition created successfully:', response);
+          },
+          error: (error) => {
+            console.error('Error creating Medical Condition:', error);
+          }
+        });
+        console.log('Medical Condition Data:', result);
+      }
+    });
+  }
 }
