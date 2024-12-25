f3d66f9c3761cb481da631e8a7164aeea2cc944e
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-12-25 21:51:39 +0000

    #72 Create Medical Condition Dialog

---
+++ src/app/components/dialog/medical-conditions/medical-conditions-dialog.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
+import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatButton} from '@angular/material/button'
+import {MedicalConditionDTO} from '../../../models/medical-condition/medicalConditionDTO';
+
+@Component({
+  selector: 'app-medical-conditions-dialog',
+  templateUrl: './medical-conditions-dialog.component.html',
+  imports: [
+    MatDialogActions,
+    MatFormField,
+    MatDialogContent,
+    MatDialogTitle,
+    ReactiveFormsModule,
+    MatInput,
+    MatButton,
+    MatFormFieldModule
+  ],
+  standalone: true
+})
+export class MedicalConditionsDialogComponent {
+  medCondForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<MedicalConditionsDialogComponent>
+  ) {
+    this.medCondForm = this.fb.group({
+      code: [''],
+      designation: [''],
+      description: [''],
+      commonSymptoms: ['']
+    });
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    if (this.medCondForm.valid) {
+      // Convert form data into an OperationType object
+      const operationData: MedicalConditionDTO = {
+        code: this.medCondForm.value.code,
+        designation: this.medCondForm.value.designation,
+        description: this.medCondForm.value.description,
+        commonSymptoms: this.medCondForm.value.commonSymptoms
+      };
+      this.dialogRef.close(operationData); // Send OperationType object back to main component
+    }
+  }
+}
