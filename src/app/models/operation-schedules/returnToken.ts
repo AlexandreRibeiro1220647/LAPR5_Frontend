7c6637bb7abd4f562b006144cadb0a8df3df25f8
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-12-25 21:37:00 +0000

    #70 AllergyDTO Model Algav Model Refactor

--- src/app/models/operation-schedules/returnToken.ts
+++ src/app/models/operation-schedules/returnToken.ts
@@ -1,5 +1,5 @@
 
 export interface ReturnToken {
   ag_op_room_better: string[][]; // [Slots][3](0 - Start Time / 1 - End Time / 2 - OpId)
-  doctors_agenda_better: { doctor: string, schedule: { start_time: string, end_time: string, surgery_id: string }[] }[]; // [DoctorID][Slots][3](0 - Start Time / 1 - End Time / 2 - TaskID)
+  lag_doctors_better: string[][][]; // [DoctorID][Slots][3](0 - Start Time / 1 - End Time / 2 - TaskID)
 }
