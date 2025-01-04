abb38d32b36d63eefc1588230779cc20ac269ba7
andre <1220633@isep.ipp.pt>
2025-01-04 14:36:29 +0000

    Medical Conditions Edit Components for staff

---
+++ src/app/components/dialog/medical-conditions/edit-for-staff/medical-conditions-dialog-staff.component.cy.ts
@@ -0,0 +1,7 @@
+import { MedicalConditionsDialogComponent } from './medical-conditions-dialog.component'
+
+describe('MedicalConditionsDialogComponent', () => {
+  it('should mount', () => {
+    cy.mount(MedicalConditionsDialogComponent)
+  })
+})
