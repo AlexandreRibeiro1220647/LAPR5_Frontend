0917a7fc37f6e121bf0241d6ab56838f3eda6d86
FernandoSilva119166611 <1191666@isep.ipp.pt>
2025-01-05 20:25:35 +0000

    Update staff.ts

--- src/app/models/staff/staff.ts
+++ src/app/models/staff/staff.ts
@@ -3,7 +3,7 @@ import {StaffStatus} from './staffstatus';
 
 export interface Staff {
   licenseNumber: string;
-  specialization: string;
+  specializationId: string;
   phone: string;
   availabilitySlots: Slot[];
   status: StaffStatus;
@@ -15,4 +15,36 @@ export interface Staff {
     };
     role: string;
   };
+}
+
+export class SpecializationDto {
+  constructor(
+    public specializationId: string,
+    public specializationDesignation: string,
+    public specializationCode: string,
+    public specializationDescription: string,
+  ) {}
+}
+export class CreateSpecializationDto {
+  constructor(
+    public specializationDesignation: string,
+    public specializationCode: string,
+    public specializationDescription: string,
+  ) {}
+}
+export class SearchSpecializationDto {
+  constructor(
+    public specializationDesignation: string,
+    public specializationCode: string,
+    public specializationDescription: string,
+  ) {}
+}
+
+
+export class Specialization {
+  constructor(
+    public specializationId: string,
+    public specializationDesignation: string,
+    public specializationCode: string,
+    public specializationDescription: string) {}
 }
