0183df3baba0f440661e246eb8bd13b660cd0325
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2025-01-05 00:35:49 +0000

    #72 E2E Tests - Edit Medical Condition Admin

---
+++ cypress/e2e/Admin-MedicalConditions/editMedicalConditionAdmin.cy.ts
@@ -0,0 +1,42 @@
+describe('Admin Allergies Component E2E Tests', () => {
+
+    it('should open the edit dialog and update a medical Condition', () => {
+
+        cy.visit('/');
+
+        cy.get('button.login_button').click();
+
+
+        cy.pause();
+
+
+        cy.get('button.right-button').contains('Medical Conditions').click();
+
+
+        cy.url().should('include', '/medicalConditions');
+
+        cy.get('tr[mat-row]').first().click({ force: true });
+
+
+        cy.get('button.edit-button').click();
+
+
+        cy.get('mat-dialog-container').should('be.visible');
+        cy.get('h2[mat-dialog-title]').should('contain', 'Medical Conditon'); // Verificar o título do modal
+
+
+        cy.get('[formControlName="designation"]').click({ force: true }).clear({ force: true }).type('Cholera 12', { force: true });
+        cy.get('[formControlName="description"]').click({ force: true }).clear({ force: true }).type('acute diarrheal infection from vibrio cholerae bacteria. - true', { force: true });
+
+
+        cy.get('button.Submit').click();
+
+
+        cy.reload();
+        cy.get('tr[mat-row]').first().within(() => {
+            cy.get('td').eq(1).should('contain', 'Cholera 12'); // Verificar coluna "Designation"
+            cy.get('td').eq(2).should('contain', 'acute diarrheal infection from vibrio cholerae bacteria. - true'); // Verificar coluna "Description"
+        });
+    });
+
+});
